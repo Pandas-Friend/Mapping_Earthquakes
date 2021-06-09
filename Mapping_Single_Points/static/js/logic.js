3921ca9f550b5fb833c89cb40d6b4338cd8d06c9
Jarred <heer.jarred@gmail.com>
2021-06-09 13:05:41 -0500

    adding files

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,10 +2,10 @@
 console.log("working");
 
 // Create the map object with a center and zoom level
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([40.7, -94.5], 14);
 console.log("map object created")
 // We create the tile layer that will be the background of our map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -14,21 +14,24 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 streets.addTo(map);
 
 console.log("streets created")
-// Creating our initial map object
-// We set the longitude, latitude, and the starting zoom level
-// This gets inserted into the div with an id of 'map'
-// var myMap = L.map("mapid", {
-//     center: [45.52, -122.67],
-//     zoom: 13
-//   });
-  
-//   // Adding a tile layer (the background map image) to our map
-//   // We use the addTo method to add objects to our map
-//   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//     tileSize: 512,
-//     maxZoom: 18,
-//     zoomOffset: -1,
-//     id: "mapbox/streets-v11",
-//     accessToken: API_KEY
-//   }).addTo(myMap);
+
+
+// // Add a marker to the map for LA
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// console.log("marker added")
+
+// // Add Circle marker for LA using circle()
+// L.circle([34.0522, -118.2437], {
+//     color: 'black',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 100
+//  }).addTo(map);
+// console.log("Circle Marker added")
+
+// Add circle using circleMarker()
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: 'ffffa1'
+}).addTo(map);
